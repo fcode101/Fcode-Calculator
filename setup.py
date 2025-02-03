700a52600429543ed8b50aed0646e13d9363b064
fcode101 <sanjay2904sri@gmail.com>
2025-02-03 17:07:39 +0530

    Initiah

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Py_Science_Calculator",
-    version="0.1",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[],
     author="F-Code 101",
@@ -10,7 +10,7 @@ setup(
     description="A Python package for scientific and electronic calculations",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/f-code101/Py_Science_Calculator",
+    url="https://github.com/fcode101/PyScience.git",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
